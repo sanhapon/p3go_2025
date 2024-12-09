082030bca0c2579fe425d0525c1022cdbcde5e28
sanhapon <sanhapon@yahoo.com>
2024-12-09 09:44:23 +0700

    add flowbite

---
+++ web/src/lib/components/SearchBox.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { Label, Input, Datepicker } from 'flowbite-svelte';
+
+    let selectedDate = null;
+</script>
+<div class='sb-container'>
+    <form class='form'>
+        <div>
+            <Input id="pickup-location" placeholder="Pick-up location" />
+        </div>
+        <div>
+            <Datepicker inline bind:value={selectedDate} />
+        </div>
+    </form>
+</div>
